5ce240445c42806b650c0c5be248053e0813de49
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-11 17:55:47 +0530

    installed @emotion/react

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 import Head from "next/head";
 
 export default function Home() {
@@ -12,6 +13,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <GradientBackgroundCon></GradientBackgroundCon>
     </>
   );
 }
