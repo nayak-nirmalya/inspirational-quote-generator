43f5f8332226f050b1bb4b51ec6f69a589dea792
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-26 14:28:37 +0530

    added func updateQuoteInfo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
@@ -7,8 +7,30 @@ import Footer from "@/components/Footer";
 import HeadMetadata from "@/components/HeadMetadata";
 import QuoteGeneratorContainer from "@/components/QuoteGeneratorContainer";
 
+import { quotesQueryName } from "@/src/graphql/queries";
+import { API } from "aws-amplify";
+
 export default function Home() {
   const [numberOfQuotes, setNumberOfQuotes] = useState<Number>(0);
+
+  const updateQuoteInfo = async () => {
+    try {
+      const response = await API.graphql<UpdateQuoteInfoData>({
+        query: quotesQueryName,
+        authMode: "AWS_IAM",
+        variables: {
+          queryName: "LIVE",
+        },
+      });
+      console.log("response", response);
+    } catch (error) {
+      console.log("error getting quote data", error);
+    }
+  };
+
+  useEffect(() => {
+    updateQuoteInfo();
+  }, []);
 
   return (
     <>
