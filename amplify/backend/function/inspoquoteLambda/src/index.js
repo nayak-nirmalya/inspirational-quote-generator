e3d0022339fa2af7c3c513a5c19a76a8befe6d31
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-26 21:11:51 +0530

    updated amplify function

--- amplify/backend/function/inspoquoteLambda/src/index.js
+++ amplify/backend/function/inspoquoteLambda/src/index.js
@@ -1,6 +1,23 @@
+/* Amplify Params - DO NOT EDIT
+	API_INSPIRATIONALQUOTES_GRAPHQLAPIIDOUTPUT
+	API_INSPIRATIONALQUOTES_QUOTEAPPDATATABLE_ARN
+	API_INSPIRATIONALQUOTES_QUOTEAPPDATATABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const sharp = require("sharp");
+const fetch = require("node-fetch");
+const path = require("path");
+const fs = require("fs");
+
 exports.handler = async (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
   return {
