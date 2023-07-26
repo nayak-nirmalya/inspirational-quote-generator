fc33b6d8b5a493be571c284134a5c10194376d6e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-26 14:31:33 +0530

    added typeguard to func

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,9 +37,21 @@ export default function Home() {
           queryName: "LIVE",
         },
       });
-      console.log("response", response);
+
+      if (!isGraphQLResultForquotesQueryName(response)) {
+        throw new Error("Unexpected response from API.graphql");
+      }
+
+      if (!response.data) {
+        throw new Error("Response data is undefined");
+      }
+
+      const receivedNumberOfQuotes =
+        response.data.quotesQueryName.items[0].quotesGenerated;
+
+      setNumberOfQuotes(receivedNumberOfQuotes);
     } catch (error) {
-      console.log("error getting quote data", error);
+      console.error("Error Getting Quotes Data", error);
     }
   };
 
